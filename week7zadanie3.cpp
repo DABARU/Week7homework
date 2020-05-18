24ceb0996710d0c69e8886af149dd7eb160b7969
DABARU <61595413+DABARU@users.noreply.github.com>
2020-05-18 15:49:56 +0300

    Add files via upload

---
+++ week7zadanie3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+void insertionSort(float arr[], int len);
+
+int main(){
+ofstream file;
+int min = -100;
+int max = 400;
+const unsigned int n = 10000;
+float arr[n];
+
+    for( unsigned int i=0; i<n; i++){
+         arr[i] = rand() % (max-min+1)+min;
+         std::cout << arr[i] << " ";
+    }
+std::cout << endl << "    " << endl;
+
+return 0;
+}
+
+
